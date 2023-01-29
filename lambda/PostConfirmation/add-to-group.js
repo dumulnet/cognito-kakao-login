47598161f98c656061dd3d1ee8a6ec77c9aaa991
김재철(Bob) <112446703+dumulnet@users.noreply.github.com>
2023-01-29 11:20:18 +0900

    Create add-to-group.js

---
+++ lambda/PostConfirmation/add-to-group.js
@@ -0,0 +1,25 @@
+const aws = require('aws-sdk');
+
+const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18',
+});
+
+/**
+ * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
+ */
+exports.handler = async (event, context, callback) => {
+  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+  const GroupName = event.request.clientMetadata.GroupName;
+  const addUserParams = {
+    GroupName,
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  };
+
+  try {
+    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+    callback(null, event);
+  } catch (e) {
+    callback(e);
+  }
+};
